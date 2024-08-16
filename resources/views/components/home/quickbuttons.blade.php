d6d58394177442db751afc3c918378cd5c7287c3
cepsmohi <ceps.mohi@gmail.com>
2024-08-16 19:42:13 +0600

    Fri Aug 16 13:06:55 +06 2024

--- resources/views/components/home/quickbuttons.blade.php
+++ resources/views/components/home/quickbuttons.blade.php
@@ -1,4 +1,12 @@
 <div class="frows gap-4">
     <x-deco.quickicon href="https://mail.google.com/" icon="70" alt="gmail"/>
+    <x-deco.quickicon href="https://calendar.google.com/" icon="63" alt="calendar"/>
+    <x-deco.quickicon href="https://www.google.com/maps" icon="73" alt="map"/>
+    <x-deco.quickicon href="https://drive.google.com/" icon="66" alt="googledrive"/>
+    <x-deco.quickicon href="https://translate.google.com/" icon="79" alt="translate"/>
     <x-deco.quickicon href="https://www.youtube.com/" icon="80" alt="youtube"/>
+    <x-deco.quickicon href="https://onedrive.live.com/" icon="8" alt="onedrive"/>
+    <x-deco.quickicon href="https://www.notion.so/" icon="259" alt="notion"/>
+    <x-deco.quickicon href="https://web.whatsapp.com/" icon="156" alt="whatsapp"/>
+    <x-deco.quickicon href="https://www.facebook.com/" icon="155" alt="facebook"/>
 </div>
