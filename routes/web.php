88b05c0f8fd4604faa62b74b2d229d41b0413747
cepsmohi <ceps.mohi@gmail.com>
2026-04-02 10:02:18 +0600

    Thu Apr  2 09:57:55 +06 2026

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('home');
+    }
     return view('welcome');
 })->name('welcome');
 Auth::routes(['register' => false]);
